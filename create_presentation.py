a92c8bb39fcba71fcb488d40d478a1b1e6932115
ShinguHan <wel2u@naver.com>
2023-04-15 08:27:51 +0900

    done 1st

---
+++ create_presentation.py
@@ -0,0 +1,56 @@
+import win32com.client
+
+def create_powerpoint_sequence_diagram(participants_info, messages_info, output_path):
+    # Create PowerPoint application, presentation, and slide
+    PPTApp = win32com.client.Dispatch("PowerPoint.Application")
+    PPTApp.Visible = True
+    presentation = PPTApp.Presentations.Add()
+    slide = presentation.Slides.Add(1, 12)  # 12 is the layout index for blank slides
+
+    # Add participants
+    left = 100
+    top = 100
+    width = 100
+    height = 50
+    spacing = 150
+    participant_shapes = []
+    for participant_info in participants_info:
+        shape = slide.Shapes.AddShape(1, left, top, width, height)
+        shape.TextFrame.TextRange.Text = participant_info["text"]
+        shape.Line.Visible = False
+        shape.Fill.ForeColor.RGB = 0xFFFFFF
+        participant_shapes.append(shape)
+
+        # Draw vertical line for each participant
+        vertical_line = slide.Shapes.AddLine(left + width/2, top + height, left + width/2, top + height + 400)
+        vertical_line.Line.ForeColor.RGB = 0x000000
+
+        left += spacing
+
+    # Add messages
+    message_top = top + height + 30
+    message_spacing = 50
+    for message_info in messages_info:
+        from_participant = participant_shapes[message_info["from"]]
+        to_participant = participant_shapes[message_info["to"]]
+
+        # Add message text
+        text_left = from_participant.Left + (abs(from_participant.Left - to_participant.Left) / 2) - 50
+        text_shape = slide.Shapes.AddShape(1, text_left, message_top, 100, 20)
+        text_shape.TextFrame.TextRange.Text = message_info["text"]
+        text_shape.Line.Visible = False
+        text_shape.Fill.ForeColor.RGB = 0xFFFFFF
+        text_shape.TextFrame.TextRange.Font.Size = 10
+        text_shape.TextFrame.AutoSize = 1
+
+        # Add horizontal line with arrow
+        horizontal_line = slide.Shapes.AddLine(from_participant.Left + width / 2, message_top + 10,
+                                               to_participant.Left + width / 2, message_top + 10)
+        horizontal_line.Line.ForeColor.RGB = 0x000000
+        horizontal_line.Line.EndArrowheadStyle = 4  # 4 is the arrowhead style
+
+        message_top += message_spacing
+
+    presentation.SaveAs(output_path)
+    presentation.Close()
+    PPTApp.Quit()
---
+++ parse_mermaid.py
@@ -0,0 +1,43 @@
+import re
+
+def parse_mermaid_sequence_diagram(mermaid_code):
+    participants_info = []
+    messages_info = []
+
+    lines = mermaid_code.strip().split('\n')
+
+    participant_pattern = re.compile(r'\s*participant\s+(\w+)(?:\s+as\s+)?(.+)?')
+    message_pattern = re.compile(r'\s*(\w+)-\>\>(\w+):\s*(.+)')
+
+    participant_to_alias = {}
+
+    for line in lines:
+        participant_match = participant_pattern.match(line)
+        message_match = message_pattern.match(line)
+
+        if participant_match:
+            participant_text = participant_match.group(1)
+            participant_alias = participant_match.group(2) if participant_match.group(2) else participant_text
+            participant_color = ''
+            participants_info.append({
+                'text': participant_alias.strip(),
+                'color': participant_color.strip(),
+            })
+            participant_to_alias[participant_text] = participant_alias.strip()
+        elif message_match:
+            from_participant = participant_to_alias.get(message_match.group(1), message_match.group(1))
+            to_participant = participant_to_alias.get(message_match.group(2), message_match.group(2))
+            try:
+                from_index = participants_info.index(next(p for p in participants_info if p['text'] == from_participant))
+                to_index = participants_info.index(next(p for p in participants_info if p['text'] == to_participant))
+            except ValueError:
+                print(f"Error: Participant not found in the sequence diagram for message: {message_match.group(3)}")
+                continue
+
+            messages_info.append({
+                'from': from_index,
+                'to': to_index,
+                'text': message_match.group(3),
+            })
+
+    return participants_info, messages_info
